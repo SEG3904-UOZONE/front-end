746c15dbcd7f35889696c1a1fee1ada776ca0dd1
Laith-Grira <lgrira12@gmail.com>
2022-11-06 12:57:09 -0500

    add icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,12 @@ import logo from '../../assets/Ottawa_resize.png'
 const Header = () => {
     return(
         <div className="top-bar">
-            <div className="wrapper"></div>
+            <div className="header">
+                <button id='homeIcon'><i className="bi bi-house-door"></i></button>
+                <button id='searchIcon'><i className="bi bi-search"></i></button>
+                <button id='languageIcon'><i className="bi bi-globe"></i></button>
+                <button id='moreIcon'><i className="bi bi-three-dots-vertical"></i></button>
+            </div>
             <div className="logo-wrapper">
                 <img className='logo' src={logo} alt="uOttawa logo" />
                 <div className="site-name"><span id='uozone'>uoZone</span></div>
