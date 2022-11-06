0b353cc05db078f36a30040ba4e65d1ea2b21b70
Laith-Grira <lgrira12@gmail.com>
2022-11-06 13:04:00 -0500

    add account icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,7 @@ const Header = () => {
                 <button id='homeIcon'><i className="bi bi-house-door"></i></button>
                 <button id='searchIcon'><i className="bi bi-search"></i></button>
                 <button id='languageIcon'><i className="bi bi-globe"></i></button>
+                <button id='accountIcon'><i className="bi bi-person-circle"></i></button>
                 <button id='moreIcon'><i className="bi bi-three-dots-vertical"></i></button>
             </div>
             <div className="logo-wrapper">
